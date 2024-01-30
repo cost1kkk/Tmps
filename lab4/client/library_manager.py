e4a3b2c16ed6c72bb6911b560725ccf88d8853f1
cost1kkk <115461925+cost1kkk@users.noreply.github.com>
2024-01-30 18:03:06 +0200

    Create library_manager.py

---
+++ lab4/client/library_manager.py
@@ -0,0 +1,22 @@
+from domain.book import Book
+from utilities.notification_service import NotificationService
+
+def main():
+    book = Book("Mara", "Ioan Slavici")
+    notification_service = NotificationService()
+    
+    book.add_observer(notification_service)
+
+    print("Book Information:")
+    print(f"Title: {book.title}")
+    print(f"Author: {book.author}\n")
+
+    # Simulate book update
+    book.update_book("Ioan Slavici")
+
+    print("\nAfter Update:")
+    print(f"Title: {book.title}")
+    print(f"Author: {book.author}")
+
+if __name__ == "__main__":
+    main()
